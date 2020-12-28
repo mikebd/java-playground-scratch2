f419d25bf1063e493bc624fc65a9e2a8694d1e14
Michael Ben-David <mikebd>
2020-12-28 17:32:55 -0500

    Add HelloControllerTest as per https://guides.micronaut.io/creating-your-first-micronaut-app/guide/index.html

---
+++ src/test/java/com/mikebd/playground/controller/HelloControllerTest.java
@@ -0,0 +1,28 @@
+package com.mikebd.playground.controller;
+
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.client.RxHttpClient;
+import io.micronaut.http.client.annotation.Client;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@MicronautTest
+class HelloControllerTest {
+    @Inject
+    @Client("/")
+    RxHttpClient client;
+
+    @Test
+    public void testHello() {
+        HttpRequest<String> request = HttpRequest.GET("/hello");
+        String body = client.toBlocking().retrieve(request);
+
+        assertNotNull(body);
+        assertEquals("Hello World", body);
+    }
+}
